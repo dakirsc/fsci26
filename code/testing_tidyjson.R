9e8d10c8456a8dfa348ec8f5dc80007c1d57e33c
dakirsc <100888458+dakirsc@users.noreply.github.com>
2026-07-24 13:54:55 -0500

    Remove testing_tidyjson since incomplete

--- code/testing_tidyjson.R
+++
@@ -1,8 +0,0 @@
-install.packages("tidyjson")
-library(tidyjson)
-tidyjson::read_json() # to specify from jsonlite
-library(purrr)
-library(repurrrsive)
-
-z <- tidyjson::read_json(got_chars)
-# GoT won't read into same tbl_json we need for this
